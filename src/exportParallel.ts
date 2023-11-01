cf69ff8e8f9bbaa8c392492fe1e272f816587cb7
IT Div Taffeta <itdiv.taffeta@gmail.com>
2023-11-01 08:43:11 +0700

    add conditional to export sharedFolders

--- src/exportParallel.ts
+++ src/exportParallel.ts
@@ -6,7 +6,8 @@ import ZimbraUserSoap from "./modules/zimbraUser";
 
 const exportAccountsParallel = async (
   zimbraURL: string,
-  zimbraToken: string
+  zimbraToken: string,
+  options: string[]
 ): Promise<void> => {
   const logFile = `export-${moment().format("YYYY-MM-DD-HH-mm-ss")}.log`;
 
@@ -17,36 +18,38 @@ const exportAccountsParallel = async (
 
   log("EXPORT", logFile, "info", `Found ${zimbraAccounts.length} accounts`);
 
-  // get folders and links for each account
-  await Promise.allSettled(
-    zimbraAccounts.map(async (account) => {
-      const userToken = await zimbraAdminSoap.delegateAuth(account.name);
-      const zimbraUserSoap = new ZimbraUserSoap(zimbraURL, userToken);
+  if (options.includes("sharedFolders")) {
+    // get folders and links for each account
+    await Promise.allSettled(
+      zimbraAccounts.map(async (account) => {
+        const userToken = await zimbraAdminSoap.delegateAuth(account.name);
+        const zimbraUserSoap = new ZimbraUserSoap(zimbraURL, userToken);
 
-      const { folders, links } = await zimbraUserSoap.getFolders();
+        const { folders, links } = await zimbraUserSoap.getFolders();
 
-      account.folders = folders;
-      account.folderLinks = links;
+        account.folders = folders;
+        account.folderLinks = links;
 
-      log(
-        account.name,
-        logFile,
-        "success",
-        `Found ${folders.length} folders and ${links.length} shared folders`
-      );
-    })
-  ).then((results) => {
-    results.forEach((result) => {
-      if (result.status === "rejected") {
         log(
-          "EXPORT",
+          account.name,
           logFile,
-          "error",
-          `Failed to get folders and links for account: ${result.reason.message}`
+          "success",
+          `Found ${folders.length} folders and ${links.length} shared folders`
         );
-      }
+      })
+    ).then((results) => {
+      results.forEach((result) => {
+        if (result.status === "rejected") {
+          log(
+            "EXPORT",
+            logFile,
+            "error",
+            `Failed to get folders and links for account: ${result.reason.message}`
+          );
+        }
+      });
     });
-  });
+  }
 
   fs.writeFileSync("accounts.json", JSON.stringify(zimbraAccounts, null, 2));
   log("All Accounts", logFile, "info", "All accounts have been exported");
--- src/exportSequential.ts
+++ src/exportSequential.ts
@@ -6,7 +6,8 @@ import ZimbraUserSoap from "./modules/zimbraUser";
 
 const exportAccountsSequential = async (
   zimbraURL: string,
-  zimbraToken: string
+  zimbraToken: string,
+  options: string[]
 ): Promise<void> => {
   const logFile = `export-${moment().format("YYYY-MM-DD-HH-mm-ss")}.log`;
 
@@ -17,30 +18,32 @@ const exportAccountsSequential = async (
 
   log("EXPORT", logFile, "info", `Found ${zimbraAccounts.length} accounts`);
 
-  // get folders and links for each account
-  for await (const account of zimbraAccounts) {
-    try {
-      const userToken = await zimbraAdminSoap.delegateAuth(account.name);
-      const zimbraUserSoap = new ZimbraUserSoap(zimbraURL, userToken);
+  if (options.includes("sharedFolders")) {
+    // get folders and links for each account
+    for await (const account of zimbraAccounts) {
+      try {
+        const userToken = await zimbraAdminSoap.delegateAuth(account.name);
+        const zimbraUserSoap = new ZimbraUserSoap(zimbraURL, userToken);
 
-      const { folders, links } = await zimbraUserSoap.getFolders();
+        const { folders, links } = await zimbraUserSoap.getFolders();
 
-      account.folders = folders;
-      account.folderLinks = links;
+        account.folders = folders;
+        account.folderLinks = links;
 
-      log(
-        account.name,
-        logFile,
-        "success",
-        `Found ${folders.length} folders and ${links.length} shared folders`
-      );
-    } catch (error: any) {
-      log(
-        "EXPORT",
-        logFile,
-        "error",
-        `Failed to get folders and links for account: ${error.message}`
-      );
+        log(
+          account.name,
+          logFile,
+          "success",
+          `Found ${folders.length} folders and ${links.length} shared folders`
+        );
+      } catch (error: any) {
+        log(
+          "EXPORT",
+          logFile,
+          "error",
+          `Failed to get folders and links for account: ${error.message}`
+        );
+      }
     }
   }
 
